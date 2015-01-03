204ff8916442c602558a6a567a8c62f6441c81e9
Phil Garner <philipgarner@alumni.soton.ac.uk>
2015-01-03 20:44:04 +0100

    Convert to testing framework as for libvar

---
+++ test-card.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 by Philip N. Garner
+ *
+ * See the file COPYING for the licence associated with this software.
+ *
+ * Author(s):
+ *   Phil Garner, September 2014
+ */
+
+#include <lv.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // Start with a vCard
+    vfile vcf("vcf");
+    var vcard = vcf.read("example3.vcf");
+    vcf.write("vcard.vcf", vcard);
+    cout << "vCard: " << vcard << endl;
+
+    vfile wab("wab");
+    var contact = wab.read("example.contact");
+    vcf.write("contact.vcf", contact);
+    cout << "Contact: " << contact << endl;
+
+    vfile ldif("ldif");
+    var ldap = ldif.read("example.ldif");
+    vcf.write("ldap.vcf", ldap);
+    cout << "LDAP: " << ldap << endl;
+    
+    return 0;
+}
